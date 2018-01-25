4c515d85e98752b8dd1cbc160cadff0c7ba8a0d1
Bipul Chandra Dev Nath <bipulcn@gmail.com>
2018-01-25 10:05:47 +0600

    updates

--- template_12/js/ang-check.js
+++ template_12/js/ang-check.js
@@ -58,7 +58,7 @@ app.controller('tmpController', ['$scope', '$cookies', '$cookieStore', '$http', 
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 1500,
+    duration: 2500,
     delay: function(el, i) { return i * 250 },
     direction: 'alternate',
     loop: true
