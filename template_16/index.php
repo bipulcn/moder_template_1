f3515fb751086d8aba678a10b3afb603c1e24cd3
Bipul Chandra Dev Nath, mac <bipulnath@Bipuls-Mac-mini.local>
2018-01-26 09:37:01 +0600

    template 16 added

---
+++ template_16/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Template DEsign</title>
+	<link rel="stylesheet" type="text/css" href="./css/colors.css">
+	<link rel="stylesheet" type="text/css" href="./css/layout.css">
+	<!-- <link rel="stylesheet" type="text/css" href="./css/layout_ext.css"> -->
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="./css/menus.css">
+	<link rel="stylesheet" type="text/css" href="./css/animate.css">
+	<link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
+</head>
+<body>
+  <div class="f_hold xcol">
+		<div class="col_8 lft bbgr">
+			<div class="logo col">
+				<h1>BASIC</h1>
+				<div>fresh organic food</div>
+			</div>
+			<ul class="menutop">
+				<li><a href='#'>Menu</a></li>
+				<li><a href='#'>Pakege</a></li>
+				<li><a href='#'>About Us</a></li>
+				<li><a href='#'>Contact</a></li>
+			</ul>
+		</div><hr class="clear">
+		<div class="f_col_12 col mrt">
+			<div class="f_hold">
+				<div class="col_8 lft mrb">
+					<h5 class="search mrt">Bea Resturent ______________</h5>
+					<h1 class="title">Three breakfast ideas to control you appetite all day</h1>
+					<div class="note">
+						Imagine sompling culinary delights from around the globe with leaving Las Vegas
+					</div>
+				</div><hr class="clear">
+				<div class="f_col_9 mrb mrt">
+					<button class="btn btn-simple rund">Book Now</button>
+				</div>
+				<div class="f_col_3 mrb mrt">
+					<div>Watch our story <i class="fa fa-location-arrow roudbtn"></i> </div>
+				</div>
+				<div class="f_col_12 xcol gry mrb">
+					<div class="f_hold mrb">
+						<div class="f_col_4 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">1 Top Breakfast list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food4.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_4 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">2 Top Lunch list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food5.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_4 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">3 Top Dinner list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food6.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="f_col_12 xcol gry mrb">
+					<div class="f_hold mrb">
+						<div class="f_col_3 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">1 Top Breakfast list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food4.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_3 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">2 Top Lunch list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food5.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="f_col_12 xcol gry mrb">
+					<div class="f_hold mrb">
+						<div class="f_col_3 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">1 Top Breakfast list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food4.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_3 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">2 Top Lunch list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food5.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_3 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">3 Top Dinner list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food6.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_3 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">4 Top Lunch list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food5.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="f_col_12 xcol gry mrb">
+					<div class="f_hold mrb">
+						<div class="f_col_4 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">1 Top Breakfast list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food4.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_4 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">2 Top Lunch list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food5.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="f_col_4 col card">
+							<div class="f_hold">
+								<h3 class="f_col_12 mrb">3 Top Dinner list</h3>
+								<div class="f_col_12 xcol msgbox">
+									<div class="f_hold">
+										<div class="f_col_5 col"><img src="./img/food6.png" alt=""></div>
+										<div class="f_col_7 col">
+											<h4>Voted Best Buffet In Las Vagas</h4>
+											<div>Imagine sompling culnary delights from around the globe</div>
+											<a href="#">View more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
