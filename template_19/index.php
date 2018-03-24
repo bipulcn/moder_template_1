ce1b9a78234ec63a7bef5f2c94499515b906f914
Bipul Chandra Dev Nath, mac <bipulnath@Bipuls-Mac-mini.local>
2018-03-24 21:06:00 +0600

    confusion 1

--- template_19/index.php
+++ template_19/index.php
@@ -2,8 +2,112 @@
 <html>
 <head>
 	<title>Template Design</title>
+	<link rel="stylesheet" type="text/css" href="./css/layout.css">
+	<link rel="stylesheet" type="text/css" href="./css/colors.css">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="./css/design.css">
 </head>
 <body>
+	<div class="f_hold header_small">
+		<div class="f_col_12 col">
+			<ul><li><a href='#'>Home</a></li><li><a href='#'>Featured</a></li><li><a href='#'>Pricing</a></li><li><a href='#'>About</a></li></ul>
+			<div class="logo"><img src="./img/logo.jpg"></div>
+		</div>		
+	</div>
+	<hr class="clear">
+	<div class="f_hold header_big">
+		<div class="f_col_12 col">
+			<ul><li><a href='#'>Home</a></li><li><a href='#'>Featured</a></li><li><a href='#'>Pricing</a></li><li><a href='#'>About</a></li></ul>
+			<div class="logo"><img src="./img/logo.jpg"></div>
+		</div>		
+	</div>
+	<div class="f_hold banner">	
+		<div class="f_col_12 col">
 
+			<div class='slogan'>-----Enjoy savings up to $350!</div>
+			<h1 class="h1_slogan">Profesional Painting Services</h1>
+
+			<button>Enquire now!</button>
+			<button>How it works?</button>
+		</div>
+	</div>
+	<div class="f_hold about">	
+		<hr class="clear">
+		<div class="f_col_12 col">
+			<h3>Why with us?</h3>
+			<div>3 Reasons to choose ProPaint Professional Painting Services.</div>
+		</div>
+		<div class="f_col_4 col">
+			<h4>Genuine Premium Nippon Paint Products</h4>
+			<div>Three breakfast ideas to control you appetite all day Imagine sompling culinary delights from around the globe with leaving Las Vegas <button>Read More</button> </div>
+		</div>
+		<div class="f_col_4 col">		
+			<h4>Certified and Highly Trained Painting Professionals</h4>
+			<div>Three breakfast ideas to control you appetite all day Imagine sompling culinary delights from around the globe with leaving Las Vegas <button>Read More</button> </div>
+		</div>
+		<div class="f_col_4 col">
+			<h4>1 year Interior Point Warranty</h4>
+			<div>Three breakfast ideas to control you appetite all day Imagine sompling culinary delights from around the globe with leaving Las Vegas <button>Read More</button> </div>
+		</div>
+	</div>
+	<div class="f_hold package">
+		<div class="f_col_4 col">
+			<table>
+			<tr><td colspan="2"><h4>Medium Package</h4></td></tr>
+				<tr><td>Studio Apt</td><td>$900</td></tr>    
+				<tr><td>3-Room</td><td>$1100</td></tr>    
+				<tr><td>4-Room</td><td>$1300</td></tr>    
+				<tr><td>5-Room</td><td>$1500</td></tr>    
+				<tr><td>5-Room Exec</td><td>$1700</td></tr>    
+				<tr><td>Monsionette</td><td>fr $1900</td></tr>
+				<tr><td colspan="2"><button>Enquire now!</button></td></tr>
+			</table>
+		</div>
+
+		<div class="f_col_4 col">
+			<table>
+			<tr><td colspan="2"><h4>Top Package</h4></td></tr>
+				<tr><td>Studio Apt</td><td>$900</td></tr>    
+				<tr><td>3-Room</td><td>$1100</td></tr>    
+				<tr><td>4-Room</td><td>$1300</td></tr>    
+				<tr><td>5-Room</td><td>$1500</td></tr>    
+				<tr><td>5-Room Exec</td><td>$1700</td></tr>    
+				<tr><td>Monsionette</td><td>fr $1900</td></tr>
+				<tr><td colspan="2"><button>Enquire now!</button></td></tr>
+			</table>
+		</div>
+
+		<div class="f_col_4 col">
+			<table>
+			<tr><td colspan="2"><h4>Standart Package</h4></td></tr>
+				<tr><td>Studio Apt</td><td>$900</td></tr>    
+				<tr><td>3-Room</td><td>$1100</td></tr>    
+				<tr><td>4-Room</td><td>$1300</td></tr>    
+				<tr><td>5-Room</td><td>$1500</td></tr>    
+				<tr><td>5-Room Exec</td><td>$1700</td></tr>    
+				<tr><td>Monsionette</td><td>fr $1900</td></tr>
+				<tr><td colspan="2"><button>Enquire now!</button></td></tr>
+			</table>
+		</div>
+	</div>
+	<div class="f_hold review">
+		<div class="f_col_4 col one">
+			<img src="./img/avatar1.svg" width="100px">
+			<h4>Guybrush Threepwood</h4>
+			<div>“a garda who detailed what life was like on the frontline”</div>
+		</div>
+		<div class="f_col_4 col two">
+			<img src="./img/avatar2.svg" width="100px">
+			<h4>William Boyd</h4>
+			<pre>Dec 30th 2016, 10:40 AM</pre>
+			<div>The journals most trigger happy poster on the report button Michael O Lainn must have been on holiday when Liam Dunne posted his “enjoy the virgins you lunatic”.</div>
+		</div>
+		<div class="f_col_4 col three">
+			<img src="./img/avatar3.svg" width="100px">
+			<h4>Teddy</h4>
+			<pre>Dec 30th 2016, 9:46 AM</pre>
+			<div>Someone was giving out a lot of soft green thumbs for some of those comments.</div>
+		</div>
+	</div>
 </body>
 </html>
