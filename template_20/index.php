837970bf11d3b71af761b3458917e8be84c45f7b
Bipul Chandra Dev Nath, mac <bipulnath@Bipuls-Mac-mini.local>
2018-03-30 00:31:34 +0600

    template 20 added

---
+++ template_20/index.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>System Management</title>
+    <!-- <link href="https://fonts.googleapis.com/css?family=Raleway|Montserrat" rel="stylesheet"> -->
+  </head>
+  <link rel="stylesheet" href="./css/colors.css">
+  <link rel="stylesheet" href="./css/layout.css">
+  <!-- <link rel="stylesheet" href="./css/layout_ext.css"> -->
+  <body>
+<div class="f_hold">
+  <div class="f_col_12 col topbar">
+   <ul class="menu" style="float: right;">
+    <li><a href='#'>LOGIN</a></li>
+    <li><a href='#'>REGISTER</a></li>
+    <li><a href='#'><span class="fa fa-shopping-basket"></span></a></li>
+    <li><a href='#'>4 ITEMS</a></li>
+   </ul>
+   <ul class="menu">
+    <li><a href='#'><span class='fa fa-eur'> </span>ENG</a></li>
+    <li><a href='#'><span class='fa fa-usd'> </span>USD</a></li>
+   </ul>
+  </div>
+  <hr class="clear">
+  <div class="f_col_12 f_hold submenu">
+    <div class="f_col_2 logo"><div class="f_hold">
+      <div class="f_col_2 col"><img src="./img/girl4.jpg" style="width: 100%;" /></div>
+      <div class="f_col_5 col"><h3>VALUE</h3></div></div>
+    </div>
+    <div class="f_col_7 col">
+      <ul class="menu" style="">
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Shop</a></li>
+        <li><a href='#'>Catalog</a></li>
+        <li><a href='#'>Page</a></li>
+        <li><a href='#'>Features</a></li>
+      </ul>
+    </div>
+    <div class="f_col_3 col"><div class="searchbox"><input type="text" name="search" placeholder="Search"><label for="search" class="fa fa-search"></label></div></div>
+  </div>
+</div>
+<div class="f_hold hf">
+  <div class="f_col_6 ban brr brl" style="background: url('./img/food1.jpg'); min-height: 100vh; ">
+    <h1>50% off</h1>
+  </div>
+  <div class="f_col_6 ban">
+    <div class="f_hold hf">
+      <div class="f_col_12 ban brr breb" style="background: url('./img/food2.jpg');"></div>
+      <div class="f_col_6 ban brr" style="background: url('./img/girl1.jpg');"></div>
+      <div class="f_col_6 ban brr" style="background: url('./img/girl5.jpg');"></div>
+    </div>
+  </div>
+</div>
+<div class="f_hold hf ofmsg">
+  <div class="f_col_4">
+    <h3>Featured Item</h3>    
+  </div>
+  <div class="f_col_4">
+    <h3>Subscribe Now</h3>    
+  </div>
+  <div class="f_col_4">
+    <h3>Best Sellers</h3>    
+  </div>
+  <div class="f_col_4 feature">
+    <div class="f_hold">
+      <div class="f_col_6 col"><img src="./img/girl4.jpg" class="circle"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col"><img src="./img/girl3.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col"><img src="./img/girl8.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col"><img src="./img/girl6.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+    </div>
+  </div>
+  <div class="f_col_4 subscribe">
+    <div class="f_hold">
+      <div class="f_col_12 col"><img src="./img/food1.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_12 col"><img src="./img/food2.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+    </div>
+  </div>
+  <div class="f_col_4 bestsells">
+    <div class="f_hold">
+      <div class="f_col_6 col"><img src="./img/girl2.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col"><img src="./img/girl1.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col"><img src="./img/girl4.jpg" class="circle"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col"><img src="./img/girl7.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+    </div>
+  </div>
+</div>
+<div class="f_hold">
+  <div class="f_col_12 col"><h3>Our Categories</h3></div>
+  <div class="f_col_3 col"><img src="./img/girl5.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_3 col"><img src="./img/girl4.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_3 col"><img src="./img/girl6.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_3 col"><img src="./img/girl1.jpg"><div><h6>Cusine Prenne</h6></div></div>
+</div>
+<div class="f_hold">
+  <div class="f_col_12 col"><h3>New Arival</h3></div>
+  <div class="f_col_2 col"><img src="./img/girl1.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl8.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl6.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl7.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl3.jpg"><div><h6>Cusine Prenne</h6></div></div>
+</div>
+<div class="f_hold">
+  <div class="f_col_12 col"><h3>From Our Blog</h3></div>
+  <div class="f_col_6 col"><h1>10</h1><h4>May</h4><img src="./img/food2.jpg"><div><button>Read More</button><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_6 col"><h1>10</h1><h4>May</h4><img src="./img/food1.jpg"><div><button>Read More</button><h6>Cusine Prenne</h6></div></div>
+</div>
+<div class="f_hold">
+  <div class="f_col_12 col"><h3>New Arival</h3></div>
+  <div class="f_col_4 col"><div><h5>Free World Wide Shipping</h5></div></div>
+  <div class="f_col_4 col"><div><h5>Free World Wide Shipping</h5></div></div>
+  <div class="f_col_4 col"><div><h5>Free World Wide Shipping</h5></div></div>
+</div>
+<div class="f_hold">
+  <div class="f_col_4">
+    <div>
+      <img src="./img/girl4.jpg" style="width: 50px;" />
+      <h3 class="Name">VALUE</h3>
+      <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+      <ul>
+        <li>PO BOx 1643 Collen Super, DKA</li>
+        <li>+39928389948</li>
+        <li>info@babyglee.com</li>
+      </ul>
+    </div>
+  </div>
+  <div class="f_col_4 col">
+    <ul>
+      <li>About us</li>
+      <li>Contact US</li>
+      <li>Faq</li>
+    </ul>
+  </div>
+  <div class="f_col_4 col">
+    facebook, google+, twitter
+  </div>
+  <div class="f_col_12 col">
+    Developed by @bdmeditech.
+  </div>
+</div>
+<img src="./img/sample.jpg" style="width: auto;">
+</body>
+<link rel="stylesheet" href="./css/css/font-awesome.min.css">
+<link rel="stylesheet" href="./css/design.css">
+<link rel="stylesheet" href="./css/structure.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/leftmenu.css">
+<link rel="stylesheet" href="./css/forms.css">
+<!-- <link rel="stylesheet" href="./css/layout_ext.css"> -->
+</html>
