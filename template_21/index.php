4cc011ad6d4f5dc4958e3f1bab10055ccc71e700
Bipul Chandra Dev Nath <bipulcn@gmail.com>
2018-11-08 14:21:20 +0600

    template 21

---
+++ template_21/index.php
@@ -0,0 +1,175 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>System Management</title>
+    <!-- <link href="https://fonts.googleapis.com/css?family=Raleway|Montserrat" rel="stylesheet"> -->
+  </head>
+  <link rel="stylesheet" href="./css/colors.css">
+  <link rel="stylesheet" href="./css/layout.css">
+  <!-- <link rel="stylesheet" href="./css/layout_ext.css"> -->
+  <body>
+    <div class="header">      
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon class="svg--sm" fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+        <polygon class="svg--lg" fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+      </svg>
+    </div>
+<div class="f_hold">
+  <div class="f_col_12 col topbar">
+   <ul class="menu" style="float: right;">
+    <li><a href='#'>LOGIN</a></li>
+    <li><a href='#'>REGISTER</a></li>
+    <li><a href='#'><span class="fa fa-shopping-basket"></span></a></li>
+    <li><a href='#'>4 ITEMS</a></li>
+   </ul>
+   <ul class="menu">
+    <li><a href='#'><span class='fa fa-eur'> </span>ENG</a></li>
+    <li><a href='#'><span class='fa fa-usd'> </span>USD</a></li>
+   </ul>
+  </div>
+  <hr class="clear">
+  <div class="f_col_12 f_hold submenu">
+    <div class="f_col_2 logo">
+      <!-- <div class="f_hold"> -->
+      <!-- <div class="f_col_2 col"> -->
+        <img src="./img/logo.png" />
+      <!-- </div> -->
+      <!-- <div class="f_col_5 col"><div>VALUE</div></div></div> -->
+    </div>
+    <div class="f_col_8 col">
+      <ul class="menu" style="">
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Shop</a></li>
+        <li><a href='#'>Catalog</a></li>
+        <li><a href='#'>Page</a></li>
+        <li><a href='#'>Features</a></li>
+      </ul>
+    </div>
+    <div class="f_col_2 col"><div class="searchbox">
+      <input type="text" name="search" placeholder="Search"><label for="search" class="fa fa-search"></label></div></div>
+  </div>
+</div>
+<div class="f_hold hf ofmsg">
+  <div class="f_col_4">
+    <h3>Featured Item</h3>    
+  </div>
+  <div class="f_col_4">
+    <h3>Special offer</h3>    
+  </div>
+  <div class="f_col_4">
+    <h3>Best Sellers</h3>    
+  </div>
+  <div class="f_col_3 feature">
+    <div class="f_hold">
+      <div class="f_col_6 col product"><img src="./img/girl4.jpg"><div><h6>Cusine Prenne</h6><div >$234</div></div></div>
+      <div class="f_col_6 col product"><img src="./img/girl3.jpg"><div><h6>Cusine Prenne</h6><div >$34</div></div></div>
+      <div class="f_col_6 col product"><img src="./img/girl8.jpg"><div><h6>Cusine Prenne</h6><div >$23</div></div></div>
+      <div class="f_col_6 col product"><img src="./img/girl6.jpg"><div><h6>Cusine Prenne</h6><div >$134</div></div></div>
+    </div>
+  </div>
+  <div class="f_col_3 subscribe">
+    <div class="f_hold">
+      <div class="f_col_12 col product">
+        <img src="./img/food1.jpg">
+        <div><h6>Cusine Prenne</h6>
+          <div>$134</div>
+          <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </div>
+        </div>
+      </div>
+      <!-- <div class="f_col_12 col product"><img src="./img/food2.jpg"><div><h6>Cusine Prenne</h6><div >$334</div></div></div> -->
+    </div>
+  </div>
+  <div class="f_col_3 bestsells">
+    <div class="f_hold">
+      <div class="f_col_6 col product"><img src="./img/girl2.jpg"><div><h6>Cusine Prenne</h6><div >$4</div></div></div>
+      <div class="f_col_6 col product"><img src="./img/girl1.jpg"><div><h6>Cusine Prenne</h6><div >$74</div></div></div>
+      <div class="f_col_6 col product"><img src="./img/girl4.jpg"><div><h6>Cusine Prenne</h6><div >$54</div></div></div>
+      <div class="f_col_6 col product"><img src="./img/girl7.jpg"><div><h6>Cusine Prenne</h6><div >$34</div></div></div>
+    </div>
+  </div>
+</div>
+<div class="f_hold catagory">
+    <div class="f_col_12 header2">      
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon class="svg--sm" fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+        <polygon class="svg--lg" fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+      </svg>
+    </div>
+  <div class="f_col_12 col"><h3>Our Categories</h3></div>
+  <div class="f_col_3 col"><img src="./img/girl5.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_3 col"><img src="./img/girl4.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_3 col"><img src="./img/girl6.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_3 col"><img src="./img/girl1.jpg"><div><h6>Cusine Prenne</h6></div></div>
+</div>
+<div class="f_hold newarive">
+  <div class="f_col_12 col"><h3>New Arival</h3></div>
+  <div class="f_col_2 col"><img src="./img/girl1.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl8.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl6.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl7.jpg"><div><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_2 col"><img src="./img/girl3.jpg"><div><h6>Cusine Prenne</h6></div></div>
+</div>
+<div class="f_hold blogs">
+  <div class="f_col_12 col"><h3>From Our Blog</h3></div>
+  <div class="f_col_6 col"><h2>10<sub>May</sub></h2><img src="./img/food2.jpg"><div><button>Read More</button><h6>Cusine Prenne</h6></div></div>
+  <div class="f_col_6 col"><h2>10<sub>April</sub></h2><img src="./img/food1.jpg"><div><button>Read More</button><h6>Cusine Prenne</h6></div></div>
+</div>
+<div class="f_hold prefoter">
+  <div class="f_col_4 col one"><div><h5>World Wide Shipping</h5></div></div>
+  <div class="f_col_4 col two"><div><h5>Its Shipping</h5></div></div>
+  <div class="f_col_4 col three"><div><h5>Free World Wide Shipping</h5></div></div>
+</div>
+<div class="f_hold footer">
+  <div class="f_col_4 col">
+    <div>
+      <div class="logo">
+        <img src="./img/girl4.jpg" style="width: 50px;" />
+        <h3 class="Name">VALUE</h3>
+      </div>
+      <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+      <div class="address">
+        <ul>
+          <li>PO BOx 1643 Collen Super, DKA</li>
+          <li>+39928389948</li>
+          <li>info@babyglee.com</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="f_col_4 col imp_link">
+    <h5>Important link</h5>
+    <ul>
+      <li>About us</li>
+      <li>Contact US</li>
+      <li>Faq</li>
+    </ul>
+  </div>
+  <div class="f_col_4 col">
+    <div class="social">
+      <img src="./img/facebook.svg">
+      <img src="./img/google-plus.svg">
+      <img src="./img/linkedin.svg">
+      <img src="./img/skype.svg">
+      <img src="./img/path.svg">
+      <img src="./img/tumblr.svg">
+    </div>
+    <div>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+  </div>
+  <div class="f_col_12 copyright">
+    Developed by @bdmeditech.
+  </div>
+</div>
+<!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<img src="./img/sample.jpg" style="width: auto;"> -->
+</body>
+<link rel="stylesheet" href="./css/css/font-awesome.min.css">
+<link rel="stylesheet" href="./css/design.css">
+<link rel="stylesheet" href="./css/structure.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/leftmenu.css">
+<link rel="stylesheet" href="./css/forms.css">
+<!-- <link rel="stylesheet" href="./css/layout_ext.css"> -->
+</html>
